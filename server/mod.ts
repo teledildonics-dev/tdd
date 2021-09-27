1efa02a7be783ea03a574c78e0086141526f7e5e
teledildonics-dev <tdd@teledildonics.dev>
2021-09-27 02:01:11 +0000

    m

--- server/mod.ts
+++ server/mod.ts
@@ -11,6 +11,7 @@ addEventListener("fetch", (event: FetchEvent) => {
       js: "application/javascript",
       jsm: "application/javascript",
       css: "text/css",
+      '': "application/javascript"
     }[pathname.split(/\./g).pop() as any];
 
     try {
