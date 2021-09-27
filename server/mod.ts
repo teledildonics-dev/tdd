96b69374fdf7199edeca98c6a1c57295d7a81f9e
teledildonics-dev <tdd@teledildonics.dev>
2021-09-27 01:59:10 +0000

    phew

--- server/mod.ts
+++ server/mod.ts
@@ -1,31 +1,32 @@
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith((async () => {
+    let { pathname } = new URL(event.request.url);
 
-addEventListener("fetch", async (event: FetchEvent) => {
-  let { pathname } = new URL(event.request.url);
-
-  if (pathname === "/") {
-    pathname = "/index.html";
-  }
-
-  let contentType: any = {
-    html: 'text/html',
-    js: 'application/javascript',
-    jsm: 'application/javascript',
-    css: 'text/css',
-  }[pathname.split(/\./g).pop() as any];
-
-  try {
-    if (contentType) {
-      return new Response(await Deno.readFile(`client/build${pathname}`), {
-        headers: {
-          "Content-Type": contentType
-        }
-      })
+    if (pathname === "/") {
+      pathname = "/index.html";
     }
-  } catch (error) {
-    console.error(error);
-  }
 
-  return new Response(`file not found: ${pathname}`, {
-    status: 404,
-  });
+    let contentType: any = {
+      html: "text/html",
+      js: "application/javascript",
+      jsm: "application/javascript",
+      css: "text/css",
+    }[pathname.split(/\./g).pop() as any];
+
+    try {
+      if (contentType) {
+        return new Response(await Deno.readFile(`client/build${pathname}`), {
+          headers: {
+            "Content-Type": contentType,
+          },
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    return new Response(`file not found: ${pathname}`, {
+      status: 404,
+    });
+  })());
 });
